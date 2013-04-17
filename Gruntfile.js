a53e1fd9ebee0198f12a1deef164b67fff2ac298
Brian McKenna <brian@precog.com>
2013-04-17 15:56:53 -0600

    Be explicit in Grunt dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,10 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('test', ['jshint', 'qunit']);
 
