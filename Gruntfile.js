558635e121f9c01465dc7903e256fc9d69aca0fb
Brian McKenna <brian@precog.com>
2013-04-30 19:35:24 -0600

    Add README to docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function(grunt) {
     },
     jsdoc : {
         dist : {
-            src: ['src/*.js'],
+            src: ['src/*.js', 'README.md'],
             options: {
                 destination: 'doc'
             }
